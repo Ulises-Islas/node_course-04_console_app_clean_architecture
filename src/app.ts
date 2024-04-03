6301f1b63c30eb6388e34c30b8d1fc557eada77b
Ulises-Islas <emyises18012@gmail.com>
2024-04-03 10:02:46 -0600

    Multiplications

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
-const message: string = 'Hello World!';
+import fs from "fs";
 
-console.log(message);
+const start = 1;
+const base = 5;
+const max = 10;
+let content: string = 
+`==================================
+    Multiplication tables (${base})
+==================================\n`;
+
+
+for (let i = start; i <= max; i++) {
+    content += `\n${base} x ${i} = ${base * i}`;
+}
+
+const outputPath = `outputs`;
+fs.mkdirSync(outputPath, { recursive: true });
+fs.writeFileSync(`${outputPath}/multiplication_table_${base}.txt`, content);
+
+console.log(content);
+
